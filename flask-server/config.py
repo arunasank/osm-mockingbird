6efb4e9f429926d9113259e8677bd29278893633
Aruna Sankaranarayanan <arunasank@src.gnome.org>
2015-05-05 23:05:35 +0530

    Add DB model, modify config file

--- flask-server/config.py
+++ flask-server/config.py
@@ -1,5 +1,9 @@
 import os
+
+#Store absolute path of this file in basedir.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+#Store path of app.db by joining it with the absolute path calculated above.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#Store path for directory in which migration data files are stored.
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
